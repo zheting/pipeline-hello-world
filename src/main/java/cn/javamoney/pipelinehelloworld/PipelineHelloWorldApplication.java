60813aacf53c68de22da42baa54e2769058ba0ce
Devin <1400902533@qq.com>
2020-02-15 09:58:40 +0800

    add java code

---
+++ src/main/java/cn/javamoney/pipelinehelloworld/PipelineHelloWorldApplication.java
@@ -0,0 +1,13 @@
+package cn.javamoney.pipelinehelloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PipelineHelloWorldApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PipelineHelloWorldApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/javamoney/pipelinehelloworld/PipelineHelloWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.javamoney.pipelinehelloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PipelineHelloWorldApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
